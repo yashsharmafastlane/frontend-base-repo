5e168ea9baae83a223305aa17889b54ac46b3abc
Yash Sharma <yashsharma@192.168.1.40>
2024-10-09 15:08:40 +0530

    feat: group pages and minor cleanup

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1 className="text-2xl font-bold">Fastlane Devs</h1>
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">Frontend base repository for Fastlane Devs</li>
+          <li className="mb-2">Tech stack: Next.js, TailwindCSS, Redux</li>
+        </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://app.clickup.com/25768430/v/dc/rjcfe-19736/rjcfe-24396"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read Our SOP's
+          </a>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn Next.js
+        </a>
+      </footer>
+    </div>
+  );
+}
